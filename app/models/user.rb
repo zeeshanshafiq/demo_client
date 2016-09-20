305bbce573270d3562c30037015943187a97dd5f
Zeeshan Shafiq <zeeshan.bscs2087@iiu.edu.pk>
2016-09-20 23:15:00 +0500

    add uniqueness of name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   # == Schema Information
   before_save :ensure_authentication_token
   has_many :articles
-
+ validates :name, uniqueness: true
   def ensure_authentication_token
     if authentication_token.blank?
       self.authentication_token = generate_authentication_token
